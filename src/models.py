ec18c0fbbb738dad54cf77666b721525df99b3d7
Moises Arevalo <moises.arevalo.sa@gmail.com>
2022-09-13 12:36:59 -0500

    beat: currency and currency value representation

--- src/models.py
+++ src/models.py
@@ -1,7 +1,21 @@
+from locale import currency
+from sqlalchemy import Column, Float, String, Integer, Date
+from sqlalchemy.orm import relationship
+from database import Base
 
+# Class to represent currency value in specific date
+class CurrencyValue(Base):
+    __tablename__='currency_value'
+    id = Column(Integer, primary_key= True, index=True)
+    date = Column(String(20))
+    currency_value = Column(Float)
+    currency_id = Column(Integer)
 
-class Current():
-    
-    current_name: str
-    current_value: float
+# Class to represent currency
+class Currency(Base):
+    __tablename__ = 'currency'
+
+    id = Column(Integer, primary_key= True, index=True)
+    currency_name = Column(String,unique=True, index=True)
+
 
